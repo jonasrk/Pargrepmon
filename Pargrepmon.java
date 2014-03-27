49446a6440f4e53ca840244996d6116390b57e3a
Jonas <jonas.kemper.biz@gmail.com>
2014-03-27 22:54:39 +0100

    words get iterated

--- Pargrepmon.java
+++ Pargrepmon.java
@@ -10,7 +10,8 @@ class Pargrepmon{
 		
 		System.out.println("Hello world!");
 		
-		List<String> tokens = new ArrayList<String>();
+		//List<String> tokens = new ArrayList<String>();
+		String[] parts = new String[4];
 		
 	    String content = null;
 	    File file = new File(args[0]);
@@ -18,34 +19,37 @@ class Pargrepmon{
 	        FileReader reader = new FileReader(file);
 	        char[] chars = new char[(int) file.length()];
 	        reader.read(chars);
-	        tokens.add(new String(chars));
+	        parts = (new String(chars)).split("\\s+");
+			System.out.println("Current parts " + parts);
 	        reader.close();
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
+		System.out.println("Current parts " + parts);
 		
-	    String content2 = null;
-	    File file2 = new File(args[1]);
-	    try {
-	        FileReader reader = new FileReader(file2);
-	        char[] chars = new char[(int) file2.length()];
-	        reader.read(chars);
-	        content2 = new String(chars);
-	        reader.close();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-		
-		Iterator<String> iterator = tokens.iterator();
-			while (iterator.hasNext()) {
-				Pattern pattern = Pattern.compile(iterator.next());
-				Matcher matcher = pattern.matcher(content2);
-				
-				while (matcher.find()) {
-				    System.out.println(pattern + " +1");
-				}
-				
-			}
+	   String content2 = null;
+	   File file2 = new File(args[1]);
+	   try {
+	       FileReader reader = new FileReader(file2);
+	       char[] chars = new char[(int) file2.length()];
+	       reader.read(chars);
+	       content2 = new String(chars);
+	       reader.close();
+	   } catch (IOException e) {
+	       e.printStackTrace();
+	   }
+	   
+	    for (int i = 0; i < parts.length; i++){
+	   		Pattern pattern = Pattern.compile(parts[i]);
+	   		Matcher matcher = pattern.matcher(content2);
+	   		
+	   		System.out.println("Current parts[i] " + parts[i]);
+	   		
+	   		while (matcher.find()) {
+	   		    System.out.println(pattern + " +1");
+	   		}
+	   		
+	   	}
 		
 	
 	}
