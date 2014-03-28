aa81726978f82666535bbd3d76a55bee578a5cb0
Jonas <jonas.kemper.biz@gmail.com>
2014-03-28 11:17:25 +0100

    output file is generated

--- Pargrepmon.java
+++ Pargrepmon.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.regex.*;
 
@@ -8,9 +6,6 @@ class Pargrepmon{
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello world!");
-		
-		//List<String> tokens = new ArrayList<String>();
 		String[] parts = new String[4];
 		
 	    String content = null;
@@ -20,53 +15,47 @@ class Pargrepmon{
 	        char[] chars = new char[(int) file.length()];
 	        reader.read(chars);
 	        parts = (new String(chars)).split("\\s+");
-			System.out.println("Current parts " + parts);
-	        reader.close();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-		System.out.println("Current parts " + parts);
+			reader.close();
+	    
 		
-	   String content2 = null;
-	   File file2 = new File(args[1]);
-	   try {
-	       FileReader reader = new FileReader(file2);
-	       char[] chars = new char[(int) file2.length()];
-	       reader.read(chars);
-	       content2 = new String(chars);
-	       reader.close();
-	   } catch (IOException e) {
-	       e.printStackTrace();
-	   }
+		String content2 = null;
+		File file2 = new File(args[1]);
+		FileReader reader2 = new FileReader(file2);
+		    char[] chars2 = new char[(int) file2.length()];
+		    reader2.read(chars2);
+		    content2 = new String(chars2);
+		    reader2.close();
 	   
-	   List<Wordcount> myList = new ArrayList<Wordcount>();
+		List<Wordcount> myList = new ArrayList<Wordcount>();
 	   
-	    for (int i = 0; i < parts.length; i++){
-	   		Pattern pattern = Pattern.compile(parts[i]);
-	   		Matcher matcher = pattern.matcher(content2);
-	   		
-	   		System.out.println("Current parts[i] " + parts[i]);
+		for (int i = 0; i < parts.length; i++){
+			Pattern pattern = Pattern.compile(parts[i]);
+			Matcher matcher = pattern.matcher(content2);
 			Wordcount this_wordcount = new Wordcount();
 			this_wordcount.word = parts[i];
 			this_wordcount.count = 0;
-	   		
-	   		while (matcher.find()) {
+			
+			while (matcher.find()) {
 				this_wordcount.count++;
-	   		    System.out.println(pattern + " +1");
-				
-	   		}
+			    
+			}
 			
 			myList.add(this_wordcount);
-	   		
-	   	}
-		
-		
-		System.out.println("My list is " + myList.toString());
-		
-		for(Wordcount this_wordcount : myList) //use for-each loop
-		{
-		    System.out.println(this_wordcount.word + ";" + this_wordcount.count);
+			
 		}
+		
+		PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+		
+		for(Wordcount this_wordcount : myList)
+		{
+		    writer.println(this_wordcount.word + ";" + this_wordcount.count);
+		}
+		
+		writer.close();
+		
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
 		
 	
 	}
