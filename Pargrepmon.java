7b6cec87a1c03cffc1322829221d8da596a78560
Jonas <jonas.kemper.biz@gmail.com>
2014-03-29 11:46:54 +0100

    synchronized keywords back. val works.

--- Pargrepmon.java
+++ Pargrepmon.java
@@ -83,7 +83,7 @@ class CentralClass{
 	List<Wordcount> output_list;
 	int number_of_processors;
 	int current_search_string_number = 0;
-	boolean list_in_use = false; //probably unnecessary
+	boolean list_in_use, current_num_in_use = false;
 	
 	public CentralClass(String[] search_strings, String second_input_string, List<Wordcount> output_list, int number_of_processors){
 		this.search_strings = search_strings;
@@ -103,12 +103,19 @@ class CentralClass{
 		// write string to result list
 		writeStringToResultList(this_wordcount);}}
 		
-	String getStringToSearchFor(){
+	synchronized String getStringToSearchFor(){
+		while (current_num_in_use){try {this.wait();} catch(InterruptedException e){e.printStackTrace();}}
+		current_num_in_use = true;
 		if (current_search_string_number < search_strings.length){
+			int return_string_number = current_search_string_number;
 			current_search_string_number++;
-			return search_strings[current_search_string_number-1];
-		}
-		else return "END_STRING";
+			current_num_in_use = false;
+			this.notify();
+			return search_strings[return_string_number];
+		} else {
+		current_num_in_use = false;
+		this.notify();
+		return "END_STRING";}
 	}
 	
 	Wordcount lookForTheStringInBuffer(String current_search_string){
@@ -122,12 +129,12 @@ class CentralClass{
 		return this_wordcount;
 	}
 	
-	void writeStringToResultList(Wordcount this_wordcount){
-		//while (list_in_use){try {this.wait();} catch(InterruptedException e){e.printStackTrace();}}
-		//list_in_use = true;
+	synchronized void writeStringToResultList(Wordcount this_wordcount){
+		while (list_in_use){try {this.wait();} catch(InterruptedException e){e.printStackTrace();}}
+		list_in_use = true;
 		output_list.add(this_wordcount);
-		//list_in_use = false;
-		//this.notify();
+		list_in_use = false;
+		this.notify();
 	}
 }
 
