4e6ec8388d03cae3fe08123ac1179885640a3df2
Jonas <jonas.kemper.biz@gmail.com>
2014-03-29 11:56:40 +0100

    removed unnecessary stuff.

--- GrepThread.java
+++ GrepThread.java
@@ -4,12 +4,10 @@ import java.util.regex.*;
 import java.lang.Thread;
 
 class GrepThread extends Thread{
-	int thread_id;
 	CentralClass centralClass;
 	
 	public GrepThread(CentralClass centralClass, int thread_id) {
-	       this.centralClass = centralClass;
-		   this.thread_id = thread_id;}
+	       this.centralClass = centralClass;}
 	
 	@Override public void run(){
-		centralClass.lookforit(thread_id);}}
+		centralClass.lookforit();}}
